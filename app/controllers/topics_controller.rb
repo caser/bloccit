2adab65347e743926b74a86d606fddd825b413b7
Casey Rosengren <nocashvaluedrumz@gmail.com>
2013-10-10 13:19:35 -0400

    Committing changes to application_helper.rb

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -34,7 +34,7 @@ class TopicsController < ApplicationController
     @topic = Topic.find(params[:id])
     authorize! :update, @topic, message: "You need to own the topic to update it."
     if @topic.update_attributes(params[:topic])
-      redirect_to @topic
+      redirect_to @topic, notice: "Topic was saved successfully."
     else
       flash[:error] = "Error in updating topic. Please try again."
       render :edit
