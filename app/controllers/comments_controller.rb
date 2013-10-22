6a1848850a43f4203ebc59ce5272155ff38f219f
Casey Rosengren <nocashvaluedrumz@gmail.com>
2013-10-22 12:04:48 -0400

    Interlude changes for registration, email permissions, and styling.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -56,7 +56,9 @@ class CommentsController < ApplicationController
 
   def send_favorite_emails
     self.post.favorites.each do |favorite|
-      FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
+      if favorite.user_id != self.user_id && favorite.user.email_favorites?
+        FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
+      end
     end
   end
   
