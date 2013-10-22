f797c743436a71c6171c82d5ce5443b8a53a1a3d
Casey Rosengren <nocashvaluedrumz@gmail.com>
2013-10-22 11:35:38 -0400

    Scoped topics for privacy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:topic_id])
+    authorize! :read, @topic, message: "You need to be signed-in to do that."
     @post = Post.find(params[:id])
     @comments = @post.comments.paginate(page: params[:page], per_page: 10)
     @comment = Comment.new
