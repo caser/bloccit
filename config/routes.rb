515cf39d7dc149f3bde0d28aef7b3cacb7d754fe
Casey Rosengren <nocashvaluedrumz@gmail.com>
2013-10-23 10:41:57 -0400

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Bloccit::Application.routes.draw do
 
+  get "posts/index"
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
-  resources :users, only: [:show] # create a route for users#show
+  resources :users, only: [:show, :index] # create a route for users#show & users#index
+
+  resource :posts, only: [:index]
 
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
       match '/up-vote', to: 'votes#up_vote', as: :up_vote
       match '/down-vote', to: 'votes#down_vote', as: :down_vote
@@ -13,6 +17,7 @@ Bloccit::Application.routes.draw do
   end
 
   match 'about' => 'welcome#about', via: :get
+  match 'posts' => 'posts#index', via: :get
   
   root to: 'welcome#index'
   
